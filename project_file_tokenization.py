68f686fb97f7aba67c0f86e7ff1c4eaa2fdf1b6d
JunGAO <bitgaojun1995@gmail.com>
2019-04-12 21:23:28 +0800

    Add project files(data&tokenization).

--- project_file_tokenization.py
+++ project_file_tokenization.py
@@ -6,14 +6,16 @@ input_file = "ProData\\2019-04-10-16-59.xls"
 df = pd.read_excel(input_file)
 str_list = list(df['Requirement'])
 basic_tokenizer = tokenization.BasicTokenizer(do_lower_case=False)
-
+id = 10000
 for req in str_list:
-    id = 10000
+
     tok_list = basic_tokenizer.tokenize(req)
     f.write('id:%d, \n' % (id))
     for tok in tok_list:
         f.write(tok+",\n")
     f.write('\n')
+    id += 1
+
 f.close()
 
 
